a62e513e53f0e6072f66e9f6b0e1b5b4581a6055
Sayeed <sksayed13@gmail.com>
2025-04-24 19:25:28 +0400

    Getting form data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,37 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    
+    /**
+     * Challenge:
+     * Add an `onSubmit` event listener on the form. Have the function
+     * simply console.log("Form submitted!") for now
+     */
+    function submitForm(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient);
+        console.log(ingredients);
+    }
+    
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={submitForm}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
-                    aria-label="add ingredient"
+                    aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button>Add Ingredient</button>
+                <button type="submit">Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
